f1855134ca9a8ad9c2a87a6134a65b3e189207d0
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 21:14:46 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+  include Dance
+  include FancyDance
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+  include Dance
+  include FancyDance
+
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+end
